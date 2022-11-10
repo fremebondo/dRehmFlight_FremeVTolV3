90e3a39648542d9dde9237ed42d72b83b2950dc6
fremebondo <gabriele.fantechi@gmail.com>
2022-11-10 14:43:55 +0100

    First hover test

--- Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
+++ Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
@@ -33,7 +33,7 @@ Everyone that sends me pictures and videos of your flying creations! -Nick
 //#define SKIP_IMU
 #define FS_MAXCOUNT 2000
 //#define MECH_SETUP_MODE
-//#define READ_EEPROM_ERRORS
+#define READ_EEPROM_ERRORS
 #define REVERSE_YAW 1
 
 //Uncomment only one receiver type
@@ -190,15 +190,15 @@ float GyroErrorZ = 10.41+0.278;
 float i_limit = 25.0;     //Integrator saturation level, mostly for safety (default 25.0)
 float maxRoll = 30.0;     //Max roll angle in degrees for angle mode (maximum ~70 degrees), deg/sec for rate mode 
 float maxPitch = 30.0;    //Max pitch angle in degrees for angle mode (maximum ~70 degrees), deg/sec for rate mode
-float maxYaw = 120.0;     //Max yaw rate in deg/sec
+float maxYaw = 220.0;     //Max yaw rate in deg/sec
 
 float Kp_roll_angle = 0.2;    //Roll P-gain - angle mode 
 float Ki_roll_angle = 0.3;    //Roll I-gain - angle mode
 float Kd_roll_angle = 0.05;   //Roll D-gain - angle mode (has no effect on controlANGLE2)
 float B_loop_roll = 0.9;      //Roll damping term for controlANGLE2(), lower is more damping (must be between 0 to 1)
-float Kp_pitch_angle = 0.8;//0.2;   //Pitch P-gain - angle mode
-float Ki_pitch_angle = 1;//0.3;   //Pitch I-gain - angle mode
-float Kd_pitch_angle = 0.2;//0.05;  //Pitch D-gain - angle mode (has no effect on controlANGLE2)
+float Kp_pitch_angle = 0.2;//0.2;   //Pitch P-gain - angle mode
+float Ki_pitch_angle = 0.5;//0.3;   //Pitch I-gain - angle mode
+float Kd_pitch_angle = 0.1;//0.05;  //Pitch D-gain - angle mode (has no effect on controlANGLE2)
 float B_loop_pitch = 0.9;     //Pitch damping term for controlANGLE2(), lower is more damping (must be between 0 to 1)
 
 float Kp_roll_rate = 0.15;    //Roll P-gain - rate mode
@@ -208,8 +208,8 @@ float Kp_pitch_rate = 0.15;   //Pitch P-gain - rate mode
 float Ki_pitch_rate = 0.2;    //Pitch I-gain - rate mode
 float Kd_pitch_rate = 0.0002; //Pitch D-gain - rate mode (be careful when increasing too high, motors will begin to overheat!)
 
-float Kp_yaw = 0.2;           //Yaw P-gain
-float Ki_yaw = 0.05;          //Yaw I-gain
+float Kp_yaw = 0.05;//0.2;           //Yaw P-gain
+float Ki_yaw = 0.02;//0.05;          //Yaw I-gain
 float Kd_yaw = 0.00015;       //Yaw D-gain (be careful when increasing too high, motors will begin to overheat!)
 
 //========================================================================================================================//
@@ -453,7 +453,7 @@ void loop() {
   //printLoopRate();      //Prints the time between loops in microseconds (expected: microseconds between loop iterations)
   //printRollAxisHover();
   //printPitchAxisHover();
- //printYawAxisHover();
+  //printYawAxisHover();
 
   //Get vehicle state
   #ifndef SKIP_IMU
@@ -524,7 +524,8 @@ void controlMixer() {
     float trimL=0.2;
     float trimR=0.2;
     float maxTilt=0.95;
-    float ff_tilt = 0.52;
+    float ff_tilt_L = 0.53;
+    float ff_tilt_R = 0.52;
     //hovering
     float mL_hov = thro_des + roll_PID; //Front left motor
     float mR_hov = thro_des - roll_PID; //Front right motor
@@ -535,8 +536,8 @@ void controlMixer() {
     //forward flight
     float mL_ff = thro_des - yaw_PID; //Front left motor
     float mR_ff = thro_des + yaw_PID; //Front right motor
-    float sL_ff = trimL + ff_tilt + roll_PID; //Tilt left servo
-    float sR_ff = trimR + ff_tilt - roll_PID; //Tilt right servo
+    float sL_ff = trimL + ff_tilt_L + roll_PID; //Tilt left servo
+    float sR_ff = trimR + ff_tilt_R - roll_PID; //Tilt right servo
     float sE_ff = 0.5 + pitch_PID;           //Elevator servo
 
     fader = floatFaderLinear2(fader,channel_6_pwm>1500,0,1,5,2,2000);
@@ -1681,7 +1682,7 @@ void loopBlink() {
       }
     else if (blinkAlternate == 0) {
       blinkAlternate = 1;
-      blink_delay = 2000000;
+      blink_delay = 1000000;
       }
   }
 }
